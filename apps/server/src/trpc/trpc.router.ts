5f027f19465724c1980a0bde1a033693bc4122c6
Daniel <etseter11@gmail.com>
2025-04-26 13:20:14 +0200

    feat(trpc): init

---
+++ apps/server/src/trpc/trpc.router.ts
@@ -0,0 +1,35 @@
+import { INestApplication, Injectable } from '@nestjs/common';
+import { TrpcService } from './trpc.service';
+import { z } from 'zod';
+import * as trpcExpress from '@trpc/server/adapters/express';
+
+@Injectable()
+export class TrpcRouter {
+    constructor(private readonly trpc: TrpcService) { }
+
+    appRouter = this.trpc.router({
+        hello: this.trpc.procedure.query(() => {
+            return { greeting: 'Hello from tRPC!' };
+        }),
+        greeting: this.trpc.procedure
+            .input(
+                z.object({
+                    name: z.string(),
+                })
+            )
+            .query(({ input }) => {
+                return { greeting: `Hello ${input.name}!` };
+            }),
+    });
+
+    async applyMiddleware(app: INestApplication) {
+        app.use(
+            `/trpc`,
+            trpcExpress.createExpressMiddleware({
+                router: this.appRouter,
+            }),
+        );
+    }
+}
+
+export type AppRouter = TrpcRouter['appRouter'];
