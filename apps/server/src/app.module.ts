52c6c124b0c2c4b8c87ad651a9c826420ec2810d
Daniel <etseter11@gmail.com>
2025-04-28 03:38:04 +0200

    feat(server)

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrpcModule } from './trpc/trpc.module';
-
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProjectModule } from './project/project.module';
+import { HttpModule } from '@nestjs/axios';
 @Module({
-  imports: [TrpcModule],
+  imports: [
+    TrpcModule,
+    AuthModule,
+    UserModule,
+    ProjectModule,
+    ConfigModule.forRoot(),
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TrpcRouter } from './trpc/trpc.router';
+import { Logger } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
 
-  app.enableCors();
+  app.use(cookieParser());
+  app.useLogger(app.get(Logger));
+  app.enableCors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+  });
 
   const trpc = app.get(TrpcRouter);
   trpc.applyMiddleware(app);
